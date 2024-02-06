6b8b9d3c665cea8e7c2688334cf0944b60cf0045
unknown <max.saraev.web@gmail.com>
2024-02-06 16:46:15 +0800

    fix: icon in case there is no image

--- src/script/createElems.js
+++ src/script/createElems.js
@@ -25,7 +25,7 @@ export const createElement =
     return element;
   };
 
-const createRow = (obj, i, url) => {
+const createRow =  (obj, i, url) => {
   const elem = createElement('tr');
   elem.classList.add('table__row');
 
@@ -33,8 +33,18 @@ const createRow = (obj, i, url) => {
     const {id, title, category, count, price, units, name, image: pic,
       discount = 0} = obj;
     const dicountAmount = ((price * count) / 100) * discount;
-  
-    elem.innerHTML = `
+
+    new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = `${url}${pic}`;
+      img.addEventListener('load', () => {
+        resolve(true);
+      });
+      img.addEventListener('error', () => {
+        reject(false);
+      });
+    }).then(trigger => {
+      elem.innerHTML = `
       <td class="table__cell table__counter">${i + 1}</td>
       <td class="table__cell table__cell_left table__cell_name" 
         data-id="${id}">
@@ -44,15 +54,38 @@ const createRow = (obj, i, url) => {
       <td class="table__cell">${units}</td>
       <td class="table__cell">${count}</td>
       <td class="table__cell">$${price}</td>
-      <td class="table__cell table__total-price">$${dicountAmount > 0 ? 
+      <td class="table__cell table__total-price">$${dicountAmount > 0 ?
         (price * count) - dicountAmount : price * count}</td>
       <td class="table__cell table__cell_btn-wrapper">
         <button data-pic="${url}${pic}"
-          class="table__btn table__btn_pic"></button>
+          class="table__btn table__btn_pic${trigger === true ?
+            '' : '-empty'}"></button>
         <button class="table__btn table__btn_edit"></button>
         <button class="table__btn table__btn_del"></button>
       </td>
     `;
+    }, trigger => {
+      elem.innerHTML = `
+      <td class="table__cell table__counter">${i + 1}</td>
+      <td class="table__cell table__cell_left table__cell_name" 
+        data-id="${id}">
+      <span class="table__cell-id">
+        id: ${id}</span>${title ? title : name}</td>
+      <td class="table__cell table__cell_left">${category}</td>
+      <td class="table__cell">${units}</td>
+      <td class="table__cell">${count}</td>
+      <td class="table__cell">$${price}</td>
+      <td class="table__cell table__total-price">$${dicountAmount > 0 ?
+        (price * count) - dicountAmount : price * count}</td>
+      <td class="table__cell table__cell_btn-wrapper">
+        <button data-pic="${url}${pic}"
+          class="table__btn table__btn_pic${trigger === true ?
+            '' : '-empty'}"></button>
+        <button class="table__btn table__btn_edit"></button>
+        <button class="table__btn table__btn_del"></button>
+      </td>
+    `;
+    });
   } else {
     console.log('Это не объект!');
   }
